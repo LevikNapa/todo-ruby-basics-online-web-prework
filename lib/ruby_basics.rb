dc76a200eca3f1c64934b2d7e213749de1d86799
Levik Naparstek <levik010@gmail.com>
2019-01-30 17:41:43 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,12 +1,12 @@
-def division(num1, num2)
+def division(7/3)
  
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name = Steve
 end
 
-def argue
+def argue(language)
   
 end
 
@@ -15,13 +15,15 @@ def greeting
 end
 
 def return_a_value
+  "Nice"
 
 end
 
 def last_evaluated_value
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party(fun = "cheese")
 
 end
